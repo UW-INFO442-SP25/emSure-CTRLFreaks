9b46f8e5d250efafd07a734bbce7d7686acb6d86
may ( <147942397+mayrockz@users.noreply.github.com>
2025-04-25 00:03:23 -0700

    add quiz landing page (html & css) and global css vars

---
+++ emSure/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import QuizLanding from './pages/quizLanding';
+
+const App = () => 
+{
+  return (
+    <div>
+        <QuizLanding />
+    </div>
+  )
+}
+
+export default App
---
+++ emSure/src/pages/quizLanding.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const QuizLanding = () => 
+{
+  return (
+
+    <div className='bg-container'>
+
+        <div className='bg-gradient'></div>
+
+        <div className='content-container'>
+
+            <img src="/imgs/book-brain.png" alt="Book Brain"></img>
+
+            <div className='quiz-page-text'>
+
+                <h1>Ready to test your health insurance knowledge?</h1>
+
+                <div className='learn-quiz-options'>
+
+                    <h3>Keep Learning</h3>
+                    <button>Take Quiz</button>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+  )
+}
+
+export default QuizLanding;
