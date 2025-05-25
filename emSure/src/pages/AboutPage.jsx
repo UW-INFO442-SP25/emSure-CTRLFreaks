16cce4a42c94b0a38eff03a4f262ad5b0b9a5c10
may ( <147942397+mayrockz@users.noreply.github.com>
2025-05-25 01:42:08 -0700

    add css for about page

--- emSure/src/pages/AboutPage.jsx
+++ emSure/src/pages/AboutPage.jsx
@@ -2,31 +2,45 @@ import React from 'react';
 
 const AboutPage = () => {
   const team = [
-    { name: "Simran Gupta", role: "Lead Researcher & Database", img: "/imgs/simran.jpg" },
-    { name: "Jason Cabusao", role: "Lead UX Designer & Developer", img: "/imgs/member2.jpg" },
-    { name: "Maryory Ajpop", role: "Lead Developer", img: "/imgs/member3.jpg" },
-    { name: "Dawn Nguyen", role: "Lead PM", img: "/imgs/dawn.jpg" },
-    { name: "Lei Ann Dela Cruz", role: "Lead UX Designer & Developer", img: "/imgs/member5.jpg" },
+    { name: "Simran Gupta", role: "Researcher", img: "/imgs/simran-pfp.png" },
+    { name: "Jason Cabusao", role: "UX Designer & Developer", img: "/imgs/simran-pfp.png" },
+    { name: "Maryory Ajpop", role: "Developer", img: "/imgs/maryory-pfp.JPG" },
+    { name: "Dawn Nguyen", role: "Project Manager", img: "/imgs/dawn-pfp.png" },
+    { name: "Lei Ann Dela Cruz", role: "UX Designer & Developer", img: "/imgs/simran-pfp.png" },
   ];
 
   return (
-    <div>
+    <div className='bg-container about'>
+      <div className='bg-gradient'></div>
       <div className="aboutpage-main-passage">
         <div className="aboutpage-mission">
           <div className="aboutpage-mission-text">
             <h1>Our mission</h1>
             <p>
               How might we support newly employed individuals in confidently understanding and selecting healthcare insurance plans, so they feel secure in their coverage? 
-              We focused on SDG Goal 3: Good Health and Well-Being, specifically with the hope of increasing access to “quality health services without facing financial hardship.” 
-              Through empowering individuals to navigate employer-based healthcare options with clarity, we aim to reduce these barriers, build confidence, and promote equitable, informed 
-              decision-making in accessing these health services.
+            </p>
+            <p>
+              We focused on tackling SDG Goal 3: Good Health and Well-Being, with the hope of increasing access to “quality health services without facing financial hardship.” 
+           
+              Through empowering individuals to navigate employer-based healthcare options with clarity, we aim to reduce these barriers, build confidence, and promote equitable, informed decision-making in accessing these health services.
             </p>
           </div>
-          <div className="aboutpage-mission-img"></div>
+          <div className="aboutpage-mission-video">
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/_J8lkDUStAw?si=7DuneENoUKeFQvhp&amp;start=1" 
+              title="YouTube video player" 
+              frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              referrerpolicy="strict-origin-when-cross-origin" 
+              allowfullscreen>
+            </iframe>
+          </div>
         </div>
 
         <div className="aboutpage-team"> 
-          <h2>Meet the team behind emSured</h2>
+          <h2>Meet the team behind <span className='emsured'>emSured</span></h2>
           <div className="aboutpage-team-members">
             {team.map((member, index) => (
                 <div className="aboutpage-member" key={index}>
